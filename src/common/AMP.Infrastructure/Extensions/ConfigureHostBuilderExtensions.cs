67920b718dcf9e2805c91bfe005c8e3d61e1eb6f
Allan Tolentino <ajbtolentino@gmail.com>
2024-09-21 20:23:17 +0800

    Updated Infrastructure (#1)
    * Update gitignore
    
    * Delete src/.DS_Store
    
    * Changed to development branch
    
    * Delete .DS_Store
    
    * Test fail
    
    * Updated publish test
    
    * AB#9 Added database connection
    
    * AB#9 Refined ef core
    
    * AP#23 Added identity
    
    * Create dotnet.yml
    
    * Test github action
    
    * AP#23 Make Identity work
    
    * AB#21 Added DuendeIdentity UI template
    
    * AB#16 Added unit of work and decorator
    
    * AB#9 Added transactional db
    
    * Add request logging
    
    * Added global exception handler
    
    * Delete src/api/AMP.EMS.API/ems.db
    
    * Update workflow name
    
    * Test build
    
    * Revert
    
    * Rename
    
    * Updated request pipeline
    
    * Updated log message
    
    * Use serilog

--- src/common/AMP.Infrastructure/Extensions/ConfigureHostBuilderExtensions.cs
+++ src/common/AMP.Infrastructure/Extensions/ConfigureHostBuilderExtensions.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Sinks.OpenTelemetry;
 
 namespace AMP.Infrastructure.Extensions;
 
@@ -10,12 +9,7 @@ public static class ConfigureHostBuilderExtensions
     public static ConfigureHostBuilder ConfigureLogger(this ConfigureHostBuilder builder)
     {
         builder.UseSerilog((context, configuration) =>
-            configuration
-            .WriteTo.OpenTelemetry("http://localhost:5341", OtlpProtocol.HttpProtobuf, null, new Dictionary<string, object>
-            {
-                { "service.name", typeof(ConfigureHostBuilderExtensions).Assembly.GetName().Name ?? "unknown_service" }
-            })
-            .ReadFrom.Configuration(context.Configuration));
+            configuration.ReadFrom.Configuration(context.Configuration));
 
         return builder;
     }
