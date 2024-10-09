aad6f01375f54d68f39ea3558e8828d44d6cb3ee
Allan Tolentino <ajbtolentino@gmail.com>
2024-10-10 01:44:40 +0800

    Revert changes

---
+++ src/ui/AMP.EMS/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  IDP_AUTHORITY_HTTPS_URL: process.env["IDP_AUTHORITY_HTTPS_URL"],
+  EMS_SPA_APIURL: process.env["EMS_SPA_APIURL"],
+  EMS_SPA_CLIENTID: process.env["EMS_SPA_CLIENTID"],
+  EMS_SPA_CLIENTSCOPE: process.env["EMS_SPA_CLIENTSCOPE"],
+  production: true
+};
