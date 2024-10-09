30f93b0610e3ab12b7853a8af929d48e6dff5550
Allan Tolentino <ajbtolentino@gmail.com>
2024-10-10 02:03:39 +0800

    Fix environments

--- src/ui/AMP.EMS/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    IDP_AUTHORITY_HTTPS_URL: 'http://localhost:6001',
-    EMS_SPA_APIURL: 'https://localhost:5443',
-    EMS_SPA_CLIENTID: 'spa',
-    EMS_SPA_CLIENTSCOPE: 'openid profile',
-    production: true,
-  };
-  
