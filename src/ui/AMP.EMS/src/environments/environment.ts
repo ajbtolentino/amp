ad7493e3a40202287a996768b07a75b90ccf2b82
Allan Tolentino <ajbtolentino@gmail.com>
2024-10-28 06:26:11 +0800

    Added offline_access

--- src/ui/AMP.EMS/src/environments/environment.ts
+++ src/ui/AMP.EMS/src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
-    IDP_AUTHORITY_HTTPS_URL: 'https://localhost:5443',
-    EMS_SPA_APIURL: 'http://localhost:6001',
-    EMS_SPA_CLIENTID: 'spa',
-    EMS_SPA_CLIENTSCOPE: 'openid profile',
-    production: true,
-  };
-  
+  IDP_AUTHORITY_HTTPS_URL: 'https://localhost:5443',
+  EMS_SPA_APIURL: 'http://localhost:6001',
+  EMS_SPA_CLIENTID: 'spa',
+  EMS_SPA_CLIENTSCOPE: 'openid profile offline_access',
+  production: true,
+};
