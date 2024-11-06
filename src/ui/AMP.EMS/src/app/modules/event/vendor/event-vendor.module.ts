02e02212242792707cc17a5f0f1bd2c2991dfcb2
Allan Tolentino <ajbtolentino@gmail.com>
2024-11-07 03:18:33 +0800

    Remove event-vendor

--- src/ui/AMP.EMS/src/app/modules/event/vendor/event-vendor.module.ts
+++ src/ui/AMP.EMS/src/app/modules/event/vendor/event-vendor.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EventVendorContractListComponent, EventVendorDetailsComponent, EventVendorListComponent, EventVendorTransactionListComponent } from '@modules/event/vendor';
+import { EventVendorContractListComponent, EventVendorListComponent, EventVendorTransactionListComponent } from '@modules/event/vendor';
 import { SharedModule } from '@modules/shared.module';
 import { EventVendorContractDetailsComponent } from './components/event-vendor-contract-details/event-vendor-contract-details.component';
 
@@ -28,7 +28,6 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     EventVendorListComponent,
-    EventVendorDetailsComponent,
     EventVendorContractListComponent,
     EventVendorTransactionListComponent,
     EventVendorContractDetailsComponent
