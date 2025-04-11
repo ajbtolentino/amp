9570b21306c78c06ae67a0711ba1140bb3eb9996
Allan Tolentino <ajbtolentino@gmail.com>
2025-04-12 01:45:32 +0800

    Fix gsap scrollTrigger

--- src/ui/AMP.EMS/src/app/modules/rsvp/invitation-details/invitation-details.component.ts
+++ src/ui/AMP.EMS/src/app/modules/rsvp/invitation-details/invitation-details.component.ts
@@ -1,13 +1,12 @@
 import { Component, Renderer2 } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RsvpService } from '@core/services';
 import { GuestInvitationService } from '@modules/event';
 import { GuestInvitation } from '@shared/models';
-import { catchError, Observable, switchMap, tap, throwError } from 'rxjs';
-
-import { Title } from '@angular/platform-browser';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { catchError, Observable, switchMap, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-invitation-details',
@@ -16,7 +15,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 })
 export class InvitationDetailsComponent {
   guestInvitation$: Observable<GuestInvitation> = new Observable<GuestInvitation>();
-  unlistener: () => void;
 
   constructor(private guestInvitationService: GuestInvitationService,
     private rsvpService: RsvpService, private route: ActivatedRoute,
@@ -35,14 +33,23 @@ export class InvitationDetailsComponent {
             return throwError(() => error);
           })
         )
-
-    this.unlistener = this.renderer2.listen("document", "load", event => {
-      console.log(`I am detecting img load`);
-    });
   }
 
   ngOnInit(): void {
     gsap.registerPlugin(ScrollTrigger);
+    // const lenis = new Lenis();
+
+    // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+    // lenis.on('scroll', ScrollTrigger.update);
+
+    // // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+    // // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+    // gsap.ticker.add((time) => {
+    //   lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+    // });
+
+    // // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+    // gsap.ticker.lagSmoothing(0);
   }
 
   refresh(): void {
@@ -58,45 +65,71 @@ export class InvitationDetailsComponent {
       observer.observe(e);
     });
 
-    let centerOffset = 0;
+    window.addEventListener('resize', () => {
+      ScrollTrigger.refresh();
+    });
 
-    if (window.innerWidth < 405) {
-      centerOffset = 50;
-    }
+    const cards: any[] = gsap.utils.toArray(".sticky-card");
+    const rotations: any[] = [-10, 10, 0];
 
-    if (window.innerHeight > 400) {
-      let cards: any[] = gsap.utils.toArray(".stack-card");
-
-      let stickDistance = 50;
-
-      let lastCardST = ScrollTrigger.create({
-        trigger: cards[cards.length - 1],
-        start: "center center",
-      });
-
-      cards.forEach((card, index) => {
-        var scale = 1 - (cards.length - index) * 0.025;
-        let scaleDown = gsap.to(card,
-          {
-            scale: scale,
-            ease: 'power1.in',
-            rotate: index + 1 !== cards.length ? -5 + ((index + 1) * 5) : 0,
-            // top: 0
-          });
-
-        ScrollTrigger.create({
-          trigger: card,
-          start: "center center",
-          end: () => lastCardST.start + stickDistance,
+    const tl = gsap.timeline(
+      {
+        scrollTrigger:
+        {
+          trigger: '.sticky-cards',
+          start: "top top",
+          end: `+=${window.innerHeight}px`,
           pin: true,
+          pinSpacing: true,
           markers: false,
-          pinSpacing: false,
-          scrub: true,
-          animation: scaleDown,
-          toggleActions: "restart none none reverse"
-        });
+          scrub: 1,
+          onUpdate: (self) => {
+            const progress = self.progress;
+            const totalCards = cards.length;
+            const progressPerCard = 1 / totalCards;
+
+            cards.forEach((card, index) => {
+              const cardStart = index * progressPerCard;
+              let cardProgress = (progress - cardStart) / progressPerCard;
+              cardProgress = Math.min(Math.max(cardProgress, 0), 1);
+
+              let yPosition = (window.innerHeight * 2) * (1 - cardProgress);
+              let xPosition = 0;
+
+              if (cardProgress === 1 && index < totalCards - 1) {
+                const remainingProgress = (progress - (cardStart + progressPerCard)) / (1 - (cardStart + progressPerCard));
+
+                if (remainingProgress > 0) {
+                  const distanceMultiplier = 1 - index * 0.05;
+                  xPosition = -window.innerWidth * 0.3 * ((distanceMultiplier * .75) * (index % 2 ? -1 : 1)) * remainingProgress;
+                  yPosition = -window.innerHeight * 0.3 * (distanceMultiplier * 0.05) * remainingProgress;
+                }
+              }
+
+              const currentRotate = cardProgress === 1 ? self.progress * rotations[index] : 0;
+
+              gsap.to(card,
+                {
+                  y: yPosition,
+                  x: xPosition,
+                  duration: 0,
+                  ease: "none"
+                });
+
+              gsap.to(card,
+                {
+                  rotate: currentRotate,
+                  duration: .25,
+                  ease: "none"
+                });
+            });
+          }
+        }
       });
-    }
+
+    tl.to('.sticky-cards', {
+      backgroundColor: document.getElementsByClassName('sticky-cards')[0].getAttribute('data-to-color') ?? ''
+    });
   }
 
   onSubmit = async (event: { guestInvitationId: string, data: any }) => {
